54e1e0e6af23ffdbae1b829c73ef1329d324789a
Abdullah <aa3541900@gmail.com>
2025-05-11 09:03:13 +0530

    login pt2

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,24 +1,35 @@
 import React from 'react'
 import Navbar from './Navbar'
 import Footer from './Footer'
+import { useSelector } from 'react-redux'
 
 
 const LoginPage = () => {
+
+  const mode = useSelector((state) => state.darkMode.value)
+
   return (
     <div>
-        <Navbar />
-        <div className='w-screen h-[90vh] flex justify-center items-center'>
-            <div className='h-[50vh] md:h-[70vh] w-[80vw] md:w-[60vw] border-2 rounded-4xl flex flex-col items-center justify-around p-5'>
-                <h1 className='font-semibold text-xl text-amber-400'>Login to continue</h1>
-                <form className='flex flex-col gap-2 h-3/4'>
-                    <label htmlFor="username">Username : </label>
-                    <input type="text" id='username' className='bg-zinc-200 rounded-lg p-2 text-lg w-[100%]' />
-                    <label htmlFor="pwd">Password : </label>
-                    <input type="password" id='pwd' className='bg-zinc-200 rounded-lg p-2 text-lg w-[100%]' />
-                </form>
-            </div>
+      <Navbar />
+      <div className={` ${mode ? 'bg-zinc-900 text-white' : 'bg-white text-black' } w-screen h-[90vh] flex justify-center items-center `}>
+        <div className='h-[50vh] md:h-[70vh] w-[80vw] md:w-[60vw] border-2 border-amber-400 rounded-4xl grid grid-rows-4'>
+          <div className='place-items-center self-center row-span-1'>
+            <h1 className='font-bold text-4xl text-amber-400'>Login</h1>
+          </div>
+          <form className='row-span-2 flex flex-col gap-2 w-full p-7'>
+            <label htmlFor="username">Username </label>
+            <input type="text" id='username' className='bg-zinc-200 rounded-lg p-2 mb-3 text-lg w-[100%]' />
+            <label htmlFor="pwd">Password</label>
+            <input type="password" id='pwd' className='bg-zinc-200 rounded-lg p-2 text-lg w-[100%]' />
+            <button className='bg-amber-400 mt-5 w-[15vw] h-max px-2 py-1 place-self-center-safe rounded-sm text-center text-black font-semibold'>Continue</button>
+          </form>
+          <div className='w-full flex justify-center h-full'>
+            
+
+          </div>
         </div>
-        <Footer />
+      </div>
+      <Footer />
     </div>
   )
 }
